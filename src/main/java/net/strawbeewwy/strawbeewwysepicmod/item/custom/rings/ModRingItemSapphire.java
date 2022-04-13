28fc0601f6fc67babe28e2d096c3e907227b1465
Strawbeewwy <Strawbeewwy@gmail.com>
2022-04-13 03:56:57 -0400

    rings now applies effect when a the holder's friend is nearby

--- src/main/java/net/strawbeewwy/strawbeewwysepicmod/item/custom/rings/ModRingItemSapphire.java
+++ src/main/java/net/strawbeewwy/strawbeewwysepicmod/item/custom/rings/ModRingItemSapphire.java
@@ -1,12 +1,11 @@
 package net.strawbeewwy.strawbeewwysepicmod.item.custom.rings;
 
-import com.google.common.collect.Multimap;
 import dev.emi.trinkets.api.SlotReference;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.attribute.EntityAttribute;
-import net.minecraft.entity.attribute.EntityAttributeModifier;
-import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
@@ -20,75 +19,65 @@ public class ModRingItemSapphire extends ModRingItem{
         super(settings);
     }
 
-    public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext tooltipContext){
+    int effectiveRadius = 0;
+    int effectAmplifier = 1;
+    int TPS = 20;
+    UUID friendUuid;
+    PlayerEntity friend;
+
+    public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
         if (stack.hasNbt()) {
             assert stack.getNbt() != null;
             if (stack.getNbt().toString().contains("quality:1")) {
                 tooltip.add(new TranslatableText("item.strawbeewwysepicmod.quality_1"));
+                effectiveRadius = 5;
+                effectAmplifier = 1;
             } else if (stack.getNbt().toString().contains("quality:2")) {
                 tooltip.add(new TranslatableText("item.strawbeewwysepicmod.quality_2"));
+                effectiveRadius = 10;
+                effectAmplifier = 2;
             } else if (stack.getNbt().toString().contains("quality:3")) {
                 tooltip.add(new TranslatableText("item.strawbeewwysepicmod.quality_3"));
+                effectiveRadius = 15;
+                effectAmplifier = 3;
             } else if (stack.getNbt().toString().contains("quality:4")) {
                 tooltip.add(new TranslatableText("item.strawbeewwysepicmod.quality_4"));
+                effectiveRadius = 20;
+                effectAmplifier = 4;
             } else if (stack.getNbt().toString().contains("quality:5")) {
                 tooltip.add(new TranslatableText("item.strawbeewwysepicmod.quality_5"));
+                effectiveRadius = 25;
+                effectAmplifier = 5;
             } else {
                 tooltip.add(new TranslatableText("item.strawbeewwysepicmod.qualityerror"));
             }
         }
     }
 
-    public Multimap<EntityAttribute, EntityAttributeModifier> getModifiers(ItemStack stack, SlotReference slot, LivingEntity entity, UUID uuid) {
-        var modifiers = super.getModifiers(stack, slot, entity, uuid);
+    public void tick(ItemStack stack, SlotReference slot, LivingEntity entity) {
 
-        //processes the quality NBT (1-5) from the base ring
-        float quality = 0;
-        if (stack.hasNbt()) {
-            assert stack.getNbt() != null;
-            if (stack.getNbt().toString().contains("quality:1")) {
-                quality = 0.01F;
-            }
-            if (stack.getNbt().toString().contains("quality:2")) {
-                quality = 0.02F;
-            }
-            if (stack.getNbt().toString().contains("quality:3")) {
-                quality = 0.03F;
-            }
-            if (stack.getNbt().toString().contains("quality:4")) {
-                quality = 0.04F;
-            }
-            if (stack.getNbt().toString().contains("quality:5")) {
-                quality = 0.05F;
+        if (friend == null || !stack.getName().asString().equals(friend.getDisplayName().asString())){
+            List<? extends PlayerEntity> players = entity.getWorld().getPlayers();
+            for (PlayerEntity player : players) {
+                if (stack.getName().asString().equals(player.getDisplayName().asString()))
+                {
+                    friend = player;
+                    friendUuid = player.getUuid();
+                }
             }
         }
 
-        int type = 0;
-        if (stack.hasNbt()) {
-            assert stack.getNbt() != null;
-            if (stack.getNbt().toString().contains("type:1")) {
-                type = 1;
-            }
-            if (stack.getNbt().toString().contains("type:2")) {
-                type = 2;
-            }
-            if (stack.getNbt().toString().contains("type:3")) {
-                type = 3;
-            }
-            if (stack.getNbt().toString().contains("type:4")) {
-                type = 4;
-            }
-            if (stack.getNbt().toString().contains("type:5")) {
-                type = 5;
+        if (friend!= null) {
+            if (friend.getPos().isInRange(entity.getPos(),effectiveRadius)) {
+                entity.addStatusEffect(new StatusEffectInstance(StatusEffects.INVISIBILITY, 5 * TPS, effectAmplifier));
+                entity.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 5 * TPS, effectAmplifier));
             }
         }
-        
-        modifiers.put(EntityAttributes.GENERIC_ARMOR, new EntityAttributeModifier(uuid, "strawbeewwysepicmod:armor",
-                0.05 + quality, EntityAttributeModifier.Operation.MULTIPLY_TOTAL));
-
-        modifiers.put(EntityAttributes.GENERIC_ARMOR, new EntityAttributeModifier(uuid, "strawbeewwysepicmod:armor",
-                type, EntityAttributeModifier.Operation.ADDITION));
-
-        return modifiers;
     }
+
+
+    public void onUnequip(ItemStack stack, SlotReference slot, LivingEntity entity) {
+        entity.clearStatusEffects();
+    }
+
 }
